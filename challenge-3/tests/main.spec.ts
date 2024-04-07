9fcdddb73f7b5191ee33acf605d587b2588c0c70
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-04-07 16:01:55 +0300

    jest ts

--- challenge-3/tests/main.spec.ts
+++ challenge-3/tests/main.spec.ts
@@ -1,34 +1,37 @@
-import { Cell, Address, toNano } from "ton-core";
-import { hex } from "../build/main.compiled.json";
 import { Blockchain } from "@ton-community/sandbox";
-import { MainContract } from "../wrappers/MainContract";
-import { send } from "process";
 import "@ton-community/test-utils";
+import { Cell, toNano } from "ton-core";
+import { hex } from "../build/master.compiled.json";
+import { MainContract } from "../wrappers/MainContract";
 
 describe("test tests", () => {
-    it("test of test", async() => {
-        const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
+  it("test of test", async () => {
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
-        const blockchain = await Blockchain.create();
+    const blockchain = await Blockchain.create();
 
-        const myContract = blockchain.openContract(
-            await MainContract.createFromConfig({}, codeCell)
-        );
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig({}, codeCell)
+    );
 
-        const senderWallet = await blockchain.treasury("sender");
+    const senderWallet = await blockchain.treasury("sender");
 
-        const sentMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(),toNano("0.05"));
+    const sentMessageResult = await myContract.sendInternalMessage(
+      senderWallet.getSender(),
+      toNano("0.05")
+    );
 
-        expect(sentMessageResult.transactions).toHaveTransaction({
-            from: senderWallet.address,
-            to: myContract.address,
-            success: true,
-        });
-
-        const getData = await myContract.getData();
-
-        expect(getData.recent_sender.toString()).toBe(senderWallet.address.toString());
-        expect(getData.number).toEqual(1); 
+    expect(sentMessageResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
     });
+
+    const getData = await myContract.getData();
+
+    expect(getData.recent_sender.toString()).toBe(
+      senderWallet.address.toString()
+    );
+    expect(getData.number).toEqual(1);
+  });
 });
-    
